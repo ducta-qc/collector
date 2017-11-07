29162fa26f08775e4a949c7f90309ffdb3a02256
ducta-qc <duc@chappiebot.com>
2017-11-08 02:41:32 +0700

    Add some tags

--- src/types/car.js
+++ src/types/car.js
@@ -4,16 +4,20 @@ export const MAP_INTENTS_TO_TAGS = {
     consultant:['O']
   },
   finance:{
-    ask_price:['O', 'PriceType', 'CarSeries', 'Location'],
+    ask_price_reason:['O'],
+    ask_price:['O', 'PriceType', 'CarSeries', 'Location', 'Source'],
+    ask_price_decay:['O', 'CarSeries', 'CarBrand'],
     ask_modify_price:['O'],
     ask_sell_price:['O'],
     ask_insurrance:['O'],
     ask_opex:['O', 'CarSeries'],
     ask_tax_fee:['O', 'BuyMethod', 'CarAdj'],
     ask_interest_rate:['O', 'BuyMethod'],
+    compare_price:['O', 'CarSeries'],
     same_price:['O'],
-    consultant_finance:['O', 'Income', 'BuyMethod', 'Possesion', 'CarSeries'],
-    consultant_price:['O', 'PriceEqual', 'PriceLower', 'PriceUpper', 'EngineTransmission', 'CarAdj']
+    consultant_finance:['O', 'Income', 'BuyMethod', 'Possesion', 'CarSeries', 'IncomeAdj'],
+    consultant_price:['O', 'PriceEqual', 'PriceLower', 'PriceUpper', 'EngineTransmission', 'CarAdj', 'CarSeries'],
+    consultant_insurrance:['O']
   },
   maintenance:{
     find:['O'],
@@ -41,8 +45,8 @@ export const MAP_INTENTS_TO_TAGS = {
   engine:{
     find:['O', 'EngineAdj'],
     consultant:['O', 'EngineFuel', 'EngineType', 'EngineTransmission'],
-    compare:['O'],
-    review:['O'],
+    compare:['O', 'EngineFuel', 'EngineAdj', 'EngineProp'],
+    review:['O', 'EngineFuel', 'EngineAdj'],
     sentiment:['O']
   },
   gear_box:{
@@ -71,39 +75,43 @@ export const MAP_INTENTS_TO_TAGS = {
     technique:['O']
   },
   fault:{
-    find:['O'],
+    find:['O', 'CarSeries', 'FaultType', 'Location'],
     consultant:['O']
   },
   car_series:{
     find:['O', 'CarBody','CarSeries', 'CarProp','CarPropValue', 'CarBrand', 'CarClass',
       'PromotionTrend', 'CarBrand', 'PriceEqual', 'PriceUpper', 'PriceLower','EngineDisplacement',
       'CarElem', 'CarElemProp', 'CarElemAdj', 'EngineFuel', 'CarAdj', 'PurposeUsage', 
-      'DateTime', 'CarYear', 'Location'],
+      'DateTime', 'CarYear', 'Location', 'Gender'],
+    ask_purpose_usage:['O','CarSeries', 'PurposeUsage', 'CarAdj'],
     ask_element:['O', 'CarElem', 'CarElemProp', 'CarSeries'],
-    ask_property:['O', 'CarSeries', 'PurposeUsage', 'CarProp', 'CarPropValue'],
-    ask_similarity:['O'],
-    compare:['O', 'CarSeries', 'CarAdj', 'PriceEqual', 'CarYear', 'PurposeUsage'],
-    compare_element:['O', 'CarElem', 'CarSeries', 'CarYear'],
+    ask_property:['O', 'CarSeries', 'PurposeUsage', 'CarProp', 'CarPropValue', 'CarBrand', 'CarElem', 'CarElemProp'],
+    ask_similarity:['O', 'CarSeries'],
+    ask_buy_procedure:['O', 'CarSeries'],
+    compare:['O', 'CarSeries', 'CarAdj', 'PriceEqual', 'CarYear', 'PurposeUsage', 'Gender'],
+    compare_same_price:['O', 'CarSeries', 'CarBody'],
+    compare_element:['O', 'CarElem', 'CarSeries', 'CarYear', 'CarElemProp'],
     compare_property:['O', 'CarSeries', 'CarProp', 'CarYear'],
-    consultant_buy:['O', 'CarAdj', 'Gender', 'CarSeries', 'PriceEqual','PriceUpper', 'PriceLower', 'EngineFuel'],
+    consultant_buy:['O', 'CarAdj', 'Gender', 'CarSeries', 'PriceEqual','PriceUpper', 
+        'PriceLower', 'EngineFuel', 'PurposeUsage', 'Source', 'CarClass', 'DateTime'],
     consultant_buy_age:['O', 'YearBorn', 'Age', 'CarProp', 'AgeType'],
     consultant_buy_five_elements:['O', 'FiveElem', 'CarProp'],
     consultant_buy_for_people:['O', 'PeopleAdj'],
-    consultant_buy_income:['O'],
+    consultant_buy_income:['O', 'Income', 'PriceEqual', 'CarSeries'],
     consultant_buy_traffic:['O'],
-    consultant_buy_purpose:['O', 'PurposeUsage'],
-    review:['O', 'CarSeries'],
-    review_element:['O'],
-    review_property:['O'],
+    consultant_buy_purpose:['O', 'PurposeUsage', 'CarBrand'],
+    review:['O', 'CarSeries', 'CarBrand'],
+    review_element:['O', 'CarElem', 'CarElemProp', 'CarSeries'],
+    review_property:['O', 'CarProp', 'CarSeries'],
     show_gallery:['O', 'CarSeries'],
     highlight_features:['O', 'CarSeries'],
-    new_arrivals:['O'],
-    sentiment:['O'],
+    new_arrivals:['O', 'DateTime', 'CarAdj'],
+    sentiment:['O', 'CarSeries'],
     sentiment_element:['O', 'CarElem', 'CarSeries'],
-    sentiment_property:['O']
+    sentiment_property:['O', 'CarProp', 'CarSeries']
   },
   car_brand:{
-    find:['O', 'CarBrandAdj', 'Location'],
+    find:['O', 'CarBrandAdj', 'Location', 'AgeType'],
     compare:['O', 'CarBrand'],
     compare_property:['O', 'CarBrandProp', 'CarBrand'],
     review:['O', 'CarBrand'],
@@ -123,7 +131,7 @@ export const MAP_INTENTS_TO_TAGS = {
   car_by_source:{
     ask_property:['O','CarProp'],
     compare:['O', 'CarSeries', 'CarBrand', 'Source', 'Location'],
-    compare_property:['O'],
+    compare_property:['O', 'Source', 'SourceProp'],
     review:['O'],
     review_property:['O'],
     sentiment:['O'],
@@ -133,6 +141,7 @@ export const MAP_INTENTS_TO_TAGS = {
     autopilot:['O'],
     career:['O'],
     club:['O'],
+    duration:['O'],
     definition:['O'],
     document:['O'],
     enviroment:['O'],
@@ -144,6 +153,7 @@ export const MAP_INTENTS_TO_TAGS = {
     smart_connect:['O'],
     sos:['O'],
     traffic_culture:['O'],
+    driving_mode:['O']
   },
   law:{
     modify:['O'],
@@ -199,7 +209,8 @@ export const MAP_INTENTS_TO_TAGS = {
     login:['O'],
     logout:['O'],
     sitemap:['O'],
-    good_bye:['O']
+    good_bye:['O'],
+    user_report:['O']
   },
   sales:{
     find:['O']
@@ -220,7 +231,7 @@ export const MAP_INTENTS_TO_TAGS = {
     check_car:['O']
   },
   test_drive:{
-    register:['O', 'CarSeries', 'Location', 'DateTime'],
+    register:['O', 'CarSeries', 'Location', 'DateTime', 'Showroom'],
     my_test_drive:['O', 'CarSeries'],
     change:['O', 'DateTime'],
     cancel:['O'],
@@ -229,7 +240,8 @@ export const MAP_INTENTS_TO_TAGS = {
     checkin:['O'],
     completed_and_i_did:['O', 'CarSeries'],
     completed_and_i_seated_in:['O'],
-    completed_but_i_did_not:['O']
+    completed_but_i_did_not:['O'],
+    consultant:['O']
   },
   other:{
     this_is_my_car:['O'],
